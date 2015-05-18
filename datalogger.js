cf41cb624b59ecfa4dfce40385b85f58952c8c88
Marco Schweighauser <marco@mailrelay.ch>
2015-05-18 11:59:57 -0700

    feat(datalogger): Add time offset for date output and use h/s for interval instead of ms.

--- datalogger.js
+++ datalogger.js
@@ -2,7 +2,8 @@ I2C1.setup({sda:B7, scl:B6});
 
 var MAX_ENTRIES = 6500;
 var MIN_BATV = 3.2;
-var INTERVAL_MS = 5 * 60 * 1000;
+var INTERVAL_S = 5 * 60;
+var TIMEZONE_OFFSET_H = -7; // OFFSET FROM UTC 
 
 var historyIndex = 0;
 var historyTimestamp;
@@ -49,29 +50,29 @@ function log() {
     return;
   }
   
+  flashGreenLED();
+  historyIndex++;
+  
+  // Set history timestamp after first measurement
+  if (historyTimestamp === undefined) {
+    historyTimestamp = Date.now();
+  }
+
   htu.getHumidity(function (x) {
-    historyHum[historyIndex] = x * 100;
+    historyHum[historyIndex-1] = x * 100;
   });
 
-  // Stagger the temperature reading by one second, to prevent conflict
+  // Stagger the temperature reading by one second, to prevent a read collision
   setTimeout(function () {
     htu.getTemperature(function (t) { 
-      historyTemp[historyIndex] = t * 100;
-
-      // Set history timestamp only after first succesfull measurement
-      if (historyTimestamp === undefined) {
-        historyTimestamp = Date.now();
-      }
-
-      historyIndex += 1;
-      flashGreenLED();
+      historyTemp[historyIndex-1] = t * 100;
     });
   }, 1000);
 }
 
 function generateDateString(i) {
-  var d = new Date(historyTimestamp + i * INTERVAL_MS);
-  return d.getMonth()+"/"+d.getDate()+"/"+d.getFullYear()+" "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+  var d = new Date(historyTimestamp + i*INTERVAL_S*1000 + TIMEZONE_OFFSET_H*3600*1000);
+  return (d.getMonth()+1)+"/"+d.getDate()+"/"+d.getFullYear()+" "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
 }
 
 function output() {
@@ -86,7 +87,7 @@ function output() {
   }
 }
 
-setInterval(log, INTERVAL_MS);
+setInterval(log, INTERVAL_S*1000);
 setWatch(flashGreenLED, BTN, true);
 setSleepIndicator(LED1);
 setDeepSleep(1);
